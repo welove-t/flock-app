e309478db9d5453b43f1307b6cf28fcbaf6bb96a
welove-t <tlove.tky@gmail.com>
2021-06-20 10:03:40 +0900

    Hero作成

--- components/lp/Hero.tsx
+++ components/lp/Hero.tsx
@@ -2,8 +2,18 @@ import Image from 'next/image';
 
 const Hero = () => {
   return (
-    <div className="relative h-screen">
+    <div className="relative h-screen flex items-center">
       <Image src="/images/hero.png" layout="fill" objectFit="cover" />
+      <div className="relative container">
+        <p className="text-5xl font-bold text-white leading-relaxed mb-6">
+          おい、イベントを
+          <br />
+          はじめよう。
+        </p>
+        <a className="bg-white font-bold rounded-full py-4 px-10 shadow">
+          アカウントを作成
+        </a>
+      </div>
     </div>
   );
 };
